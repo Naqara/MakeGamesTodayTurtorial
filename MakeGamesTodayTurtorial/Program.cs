4759f0f86ee96da51f7df8c018811c41780ebf2a
Michal Studencki <studi2005@gmail.com>
2020-09-21 22:12:25 +0200

    Dodaj pliki projektów.

---
+++ MakeGamesTodayTurtorial/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MakeGamesTodayTurtorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who are you, Stranger?");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("I will still call you Stranger, then.");
+                name = "Stranger";
+            }
+            else if (name.ToLower() == "stranger")
+            {
+                Console.WriteLine("Ha! I knew it!");
+            }
+            Console.WriteLine($"Where are you from, {name}?");
+            string place = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(place))
+            {
+                Console.WriteLine("You are not too talkative, are you?");
+                place = "Nowwhere";
+            }
+            Console.WriteLine($"Welcom to Maoi, {name} from {place}!");
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey(true); //true ukrywa klawisz naciśniety przez użytkownika.
+
+            string[] level = {
+            "#########",
+            "#    #  #",
+            "#   ##  #",
+            "#    #  #",
+            "#    #  #",
+            "#       #",
+            "#    #  #",
+            "#########"
+            };
+            string[] scroll =
+            {
+    "     _______________,",
+    "()==(              (@==()",
+    "     '______________'|",
+    "       |             |",
+    "       |             |",
+    "     __)_____________|    ",
+    "()==(               (@==()",
+    "     '--------------'     ",
+    "PjP"
+            };
+            Console.Clear();
+            Console.WriteLine("Wanna see the map? Press any key until it is revealed...");
+
+            int scrollHalf = scroll.Length / 2;
+            for (int i = 0; i < scrollHalf; i++)
+            {
+                Console.WriteLine(scroll[i]);
+            }
+            int nextMapRowPosition = Console.CursorTop;
+            foreach (string row in level)
+            {
+                Console.SetCursorPosition(0, nextMapRowPosition);
+                Console.WriteLine($"       |  {row}  |");
+                for (int i = scrollHalf; i < scroll.Length; i++)
+                {
+                    Console.WriteLine(scroll[i]);
+                }
+                nextMapRowPosition++;
+                Console.ReadKey(true);
+            }
+
+
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
